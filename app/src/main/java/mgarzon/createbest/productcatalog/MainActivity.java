4a61ca0793c6f2be5ac74f4f4474f4da09b254cb
createbest <createbest2019@gmail.com>
2019-05-15 10:55:02 -0400

    creating a branch for my project file

--- app/src/main/java/mgarzon/createbest/productcatalog/MainActivity.java
+++ app/src/main/java/mgarzon/createbest/productcatalog/MainActivity.java
@@ -41,7 +41,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        databaseProducts = FirebaseDatabase.getInstance().getReference("products");
+
         editTextName = (EditText) findViewById(R.id.editTextName);
         editTextPrice = (EditText) findViewById(R.id.editTextPrice);
         listViewProducts = (ListView) findViewById(R.id.listViewProducts);
@@ -76,7 +76,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
 
-                //clearing the previous artist list
+                //clearing the previous list
                 products.clear();
 
                 //iterating through all the nodes
@@ -141,7 +141,7 @@ public class MainActivity extends AppCompatActivity {
 
     private void updateProduct(String id, String name, double price) {
         //getting the specified product reference
-        DatabaseReference dR = FirebaseDatabase.getInstance().getReference("products").child(id);
+        DatabaseReference dR = FirebaseDatabase.getInstance().getReference("createbest-product-catalog").child(id);
         //updating product
         Product product = new Product(id, name, price);
         dR.setValue(product);
@@ -150,7 +150,7 @@ public class MainActivity extends AppCompatActivity {
 
     private boolean deleteProduct(String id) {
         //getting the specified product reference
-        DatabaseReference dR = FirebaseDatabase.getInstance().getReference("products").child(id);
+        DatabaseReference dR = FirebaseDatabase.getInstance().getReference("createbest-product-catalog").child(id);
         //removing prodct
         dR.removeValue();
         Toast.makeText(getApplicationContext(), "Product Deleted", Toast.LENGTH_LONG).show();
